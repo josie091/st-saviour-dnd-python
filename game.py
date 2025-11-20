a100fe066368620098f064eefed33b1907bf3855
Josie <jjurado28@stsaviour.org>
2025-11-20 13:12:14 -0500

    lab 3

--- game.py
+++ game.py
@@ -1,7 +1,13 @@
 import random
 import time
+import sys
 
 from draw import draw_d20, draw_d6, draw_d4
+
+def check_player_alive(lives: int) -> None:
+    if lives <= 0:
+        print('You died!')
+        sys.exit()
 
 def print_dramatic_text(text: str, delay=0.1):
     for char in text:
@@ -16,12 +22,64 @@ if __name__ == '__main__':
     # any buffs / debuffs?
     # any critical success / failure?
 
-    name = input('Name: ')
-    role = input('Role: ')
+    print_dramatic_text('Welcome to Josies trivia game!!')
 
-    print('Your name is ' + name + ' and your role is ' + role + '.')
-    print_dramatic_text('Our adventure begins in a shady tavern ...')
+    lives = 5
+    answer = input('Question 1: What is the slowest animal on the Earth?\n')
+    if answer.lower() == 'slug':
+        print_dramatic_text('correct')
+    else:
+        print_dramatic_text('Incorrect')
+        lives -= 1
+        check_player_alive(lives)
 
-    input('Press Enter to roll a d20.')
-    roll = random.randint(1, 20)
-    draw_d20(roll)
+    answer = input('Question 2: What is the fastest animal on earth?\n')
+    if answer.lower() == 'hawk':
+        print_dramatic_text('correct')
+    else:
+        print_dramatic_text('Incorrect')
+        lives -= 1
+        check_player_alive(lives)
+
+    answer = input('Question 3:What is the prettiest animal that can fly?\n')
+    if answer.lower() == 'butterfly':
+        print_dramatic_text('correct')
+    else:
+        print_dramatic_text('Incorrect')
+        lives -= 1
+        check_player_alive(lives)
+
+    questions = [
+        'Question 4: Do you like food?',
+        'Question 4: Would you ever want to be a zombie?',
+        'Question 4: Do you sing in the shower?'
+    ]
+ 
+    answers = [
+        'yes',
+        'yes',
+        'yes' 
+    ]
+
+    r = random.randint(0, 2)
+    answer = input(questions[r] + ' ')
+    if answer.lower() == answers[r]:
+        print_dramatic_text('correct!')
+    else:
+        print_dramatic_text('incorrect ...')
+        lives -= 1
+        check_player_alive(lives)
+
+    # add more questions?
+
+    print_dramatic_text('Congrats you finished the game !! :( ')
+ 
+    
+
+          
+      
+
+
+
+
+
